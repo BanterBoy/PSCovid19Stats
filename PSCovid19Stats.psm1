76e6fdaf08865310867395e97ca5122d449009b2
Luke Leigh <luke.leigh@gmail.com>
2020-03-31 13:15:03 +0100

    Update PSCovid19Stats.psm1

--- PSCovid19Stats.psm1
+++ PSCovid19Stats.psm1
@@ -131,6 +131,7 @@ function Get-CovidCountryStats {
         GitHubGist: https://gist.github.com/BanterBoy
     
     #>
+    
 
     [CmdletBinding(DefaultParameterSetName = 'ParameterSet1',
         SupportsShouldProcess = $false,
@@ -145,7 +146,13 @@ function Get-CovidCountryStats {
         [ValidateNotNull()]
         [ValidateNotNullOrEmpty()]
         [OutputType([String])]
-        [ArgumentCompleter( { 'AF', 'AX', 'AL', 'DZ', 'AS', 'AD', 'AO', 'AI', 'AQ', 'AG', 'AR', 'AM', 'AW', 'AU', 'AT', 'AZ', 'BS', 'BH', 'BD', 'BB', 'BY', 'BE', 'BZ', 'BJ', 'BM', 'BT', 'BO', 'BA', 'BW', 'BV', 'BR', 'IO', 'BN', 'BG', 'BF', 'BI', 'KH', 'CM', 'CA', 'CV', 'KY', 'CF', 'TD', 'CL', 'CN', 'CX', 'CC', 'CO', 'KM', 'CG', 'CD', 'CK', 'CR', 'CI', 'HR', 'CU', 'CY', 'CZ', 'DK', 'DJ', 'DM', 'DO', 'EC', 'EG', 'SV', 'GQ', 'ER', 'EE', 'ET', 'FK', 'FO', 'FJ', 'FI', 'FR', 'GF', 'PF', 'TF', 'GA', 'GM', 'GE', 'DE', 'GH', 'GI', 'GR', 'GL', 'GD', 'GP', 'GU', 'GT', 'GG', 'GN', 'GW', 'GY', 'HT', 'HM', 'VA', 'HN', 'HK', 'HU', 'IS', 'IN', 'ID', 'IR', 'IQ', 'IE', 'IM', 'IL', 'IT', 'JM', 'JP', 'JE', 'JO', 'KZ', 'KE', 'KI', 'KR', 'KW', 'KG', 'LA', 'LV', 'LB', 'LS', 'LR', 'LY', 'LI', 'LT', 'LU', 'MO', 'MK', 'MG', 'MW', 'MY', 'MV', 'ML', 'MT', 'MH', 'MQ', 'MR', 'MU', 'YT', 'MX', 'FM', 'MD', 'MC', 'MN', 'ME', 'MS', 'MA', 'MZ', 'MM', 'NA', 'NR', 'NP', 'NL', 'AN', 'NC', 'NZ', 'NI', 'NE', 'NG', 'NU', 'NF', 'MP', 'NO', 'OM', 'PK', 'PW', 'PS', 'PA', 'PG', 'PY', 'PE', 'PH', 'PN', 'PL', 'PT', 'PR', 'QA', 'RE', 'RO', 'RU', 'RW', 'BL', 'SH', 'KN', 'LC', 'MF', 'PM', 'VC', 'WS', 'SM', 'ST', 'SA', 'SN', 'RS', 'SC', 'SL', 'SG', 'SK', 'SI', 'SB', 'SO', 'ZA', 'GS', 'ES', 'LK', 'SD', 'SR', 'SJ', 'SZ', 'SE', 'CH', 'SY', 'TW', 'TJ', 'TZ', 'TH', 'TL', 'TG', 'TK', 'TO', 'TT', 'TN', 'TR', 'TM', 'TC', 'TV', 'UG', 'UA', 'AE', 'GB', 'US', 'UM', 'UY', 'UZ', 'VU', 'VE', 'VN', 'VG', 'VI', 'WF', 'EH', 'YE', 'ZM', 'ZW' }) ]
+        [ArgumentCompleter( {
+                $Content = Invoke-RestMethod -Uri 'https://datahub.io/core/country-list/r/data.json'
+                $ISOCodes = $Content | Sort-Object -Property Code
+                foreach ($Code in $ISOCodes) {
+                    $Code.Code
+                }
+            }) ]
         [string]
         $Country
     )
@@ -506,4 +513,47 @@ function Get-CovidWorldStats {
 
     }
 
+}
+
+function Show-CountryCodes {
+    <#
+        .SYNOPSIS
+        Command used to display a list of Country Codes
+    
+        .DESCRIPTION
+        This Command can be used to display a list of standard Country Codes from the standard [ISO Country Codes](https://en.wikipedia.org/wiki/List_of_ISO_3166_country_codes)
+    
+        .INPUTS
+        None. You cannot pipe objects to Show-CountryCodes
+    
+        .OUTPUTS
+        System.String. Show-CountryCodes returns a string with all of the ISO Country Codes.
+    
+        .EXAMPLE
+        PS C:\GitRepos> Show-CountryCodes
+        Code Name
+        ---- ----
+        AF   Afghanistan
+        AX   Åland Islands
+        AL   Albania
+        DZ   Algeria
+        AS   American Samoa
+        AD   Andorra
+        AO   Angola
+        AI   Anguilla
+        AQ   Antarctica
+        ...............
+
+        .LINK
+        https://github.com/BanterBoy/PSCovid19Stats/wiki/Show-CountryCodes
+    
+        .NOTES
+        Author:     Luke Leigh
+        Website:    https://blog.lukeleigh.com/
+        LinkedIn:   https://www.linkedin.com/in/lukeleigh/
+        GitHub:     https://github.com/BanterBoy/
+        GitHubGist: https://gist.github.com/BanterBoy
+    
+    #>
+    Invoke-RestMethod -Uri 'https://datahub.io/core/country-list/r/data.json'
 }
