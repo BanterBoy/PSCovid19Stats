284a628aa23cefc98064194e097866b5424d18ce
Luke Leigh <luke.leigh@gmail.com>
2020-06-03 20:36:42 +0100

    Update AllApiEndpoints.ps1

--- v2/Functions/AllApiEndpoints.ps1
+++ v2/Functions/AllApiEndpoints.ps1
@@ -1,16 +1,12 @@
 V2
 
 #ALL
-$AllHeaders = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
-$AllHeaders.Add("Cookie", "__cfduid=d6907f091c38e985d84bd05e1faf548a61585349147")
 $AllResponses = Invoke-RestMethod 'https://corona.lmao.ninja/v2/all'
 
 
-#ALLDate
-$AllDateHeaders = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
-$AllDateHeaders.Add("Cookie", "__cfduid=d6907f091c38e985d84bd05e1faf548a61585349147")
-$NumberDays = 1
-$date = (Get-Date).AddDays(-$NumberDays)
-$AllDateResponses = Invoke-RestMethod 'https://corona.lmao.ninja/v2/all?yesterday=$date'
+#ALLYesterday
+$AllDateResponses = Invoke-RestMethod 'https://corona.lmao.ninja/v2/all?yesterday='
 
+true
+false
 
