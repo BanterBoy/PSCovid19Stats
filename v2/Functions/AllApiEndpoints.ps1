a1e6a06a0ef36bcabd6289486ac0dd28aa9f8bd8
Luke Leigh <luke.leigh@gmail.com>
2020-06-17 12:08:04 +0100

    Update AllApiEndpoints.ps1

--- v2/Functions/AllApiEndpoints.ps1
+++ v2/Functions/AllApiEndpoints.ps1
@@ -1,5 +1,7 @@
 V2
 
+Get global stats: cases, deaths, recovered, time last updated, and active cases. Data is updated every 10 minutes.
+
 #ALL
 $AllResponses = Invoke-RestMethod 'https://corona.lmao.ninja/v2/all'
 
