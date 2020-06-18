83298dbbde501a5ff83e70f05bb31833842b2688
Luke Leigh <luke.leigh@gmail.com>
2020-06-18 01:46:07 +0100

    Update Get-CovidWorldStats.ps1

--- v2/Functions/Public/Get-CovidWorldStats.ps1
+++ v2/Functions/Public/Get-CovidWorldStats.ps1
@@ -1,68 +1,153 @@
 function Get-CovidWorldStats {
 
     <#
-
         .SYNOPSIS
-        Command used to extract Data for all Returns all total cases, recovery, and deaths from the NovelCOVID API (github.com/NovelCOVID/API)
-
+        Command used to extract data (Global Stats) from the NovelCOVID API (github.com/NovelCOVID/API).
+    
         .DESCRIPTION
-        Command used to extract John HOpkins CSSE Data from the NovelCOVID API (github.com/NovelCOVID/API)
-        Returns all total cases, recovery, and deaths.
-
+        This command can be used to extract data (Global Stats) from the NovelCOVID API (github.com/NovelCOVID/API). It will extract the global statistics and includes: cases, deaths, recovered, time last updated, and active cases. This data is updated every 10 minutes.
+    
         .INPUTS
         None. You cannot pipe objects to Get-CovidWorldStats
-
+    
         .OUTPUTS
-        System.String. Get-CovidWorldStats returns a string of all total cases, recovery, and deaths.
+        System.String. Get-CovidWorldStats returns a string with all of the stats for every country affected with Covid-19.
+    
+        .EXAMPLE
+        Get-CovidWorldStats
+
+        deathsPerOneMillion    : 57.3
+        activePerOneMillion    : 451.07
+        oneDeathPerPeople      : 0
+        deaths                 : 446666
+        oneTestPerPeople       : 0
+        critical               : 54576
+        oneCasePerPeople       : 0
+        criticalPerOneMillion  : 7.04
+        active                 : 3498202
+        recovered              : 4341839
+        tests                  : 117370255
+        casesPerOneMillion     : 1063
+        updated                : 1592390360372
+        todayRecovered         : 39052
+        recoveredPerOneMillion : 559.85
+        testsPerOneMillion     : 15134.07
+        todayCases             : 35483
+        todayDeaths            : 1478
+        population             : 7755366723
+        affectedCountries      : 215
+        cases                  : 8286707
 
         .EXAMPLE
-        PS C:\GitRepos> Get-CovidWorldStats
-        Active    : 536625
-        Recovered : 151312
-        Deaths    : 33966
-        Cases     : 721903
-        Updated   : 30/03/2020 02:08:07
+        Get-CovidWorldStats -yesterday
+
+        deathsPerOneMillion    : 57.3
+        activePerOneMillion    : 451.14
+        oneDeathPerPeople      : 0
+        deaths                 : 446667
+        oneTestPerPeople       : 0
+        critical               : 54576
+        oneCasePerPeople       : 0
+        criticalPerOneMillion  : 7.04
+        active                 : 3498778
+        recovered              : 4341848
+        tests                  : 117386313
+        casesPerOneMillion     : 1063
+        updated                : 1592390960163
+        todayRecovered         : 39061
+        recoveredPerOneMillion : 559.85
+        testsPerOneMillion     : 15136.14
+        todayCases             : 36069
+        todayDeaths            : 1479
+        population             : 7755366723
+        affectedCountries      : 215
+        cases                  : 8287293
 
         .LINK
         https://github.com/BanterBoy/PSCovid19Stats/wiki/Get-CovidWorldStats
-
+    
         .NOTES
-        Author: Luke Leigh  
-        Website: https://blog.lukeleigh.com  
-        LinkedIn: https://www.linkedin.com/in/lukeleigh  
-        GitHub: https://github.com/BanterBoy  
-        GitHubGist: https://gist.github.com/BanterBoy  
-
+        Author:     Luke Leigh
+        Website:    https://blog.lukeleigh.com/
+        LinkedIn:   https://www.linkedin.com/in/lukeleigh/
+        GitHub:     https://github.com/BanterBoy/
+        GitHubGist: https://gist.github.com/BanterBoy
+    
     #>
+
+    [CmdletBinding(DefaultParameterSetName = 'Default',
+        SupportsShouldProcess = $false,
+        PositionalBinding = $false,
+        ConfirmImpact = 'Medium')]
+    param(
+        [Parameter(Mandatory = $false,
+            ValueFromPipeline = $false,
+            ValueFromPipelineByPropertyName = $false,
+            ParameterSetName = 'Default')]
+        [switch]$yesterday
+    )
 
 
     BEGIN {
-    
+        if ( $yesterday ) { $URI = "https://corona.lmao.ninja/v2/all?yesterday" }
+        else { $URI = "https://corona.lmao.ninja/v2/all" }
     }
 
     PROCESS {
-        $AllHeaders = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
-        $AllHeaders.Add("Cookie", "__cfduid=d6907f091c38e985d84bd05e1faf548a61585349147")
-        $AllResponses = Invoke-RestMethod 'https://corona.lmao.ninja/v2/all' -Method 'GET' -Headers $AllHeaders -Body $body
-        $EpochStart = Get-Date "01/01/1970 00:00:00"
 
-        foreach ($Response in $AllResponses) {
+        $AllStatsHeaders = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+        $AllStatsHeaders.Add("Cookie", "__cfduid=da03e729af67bf9c91ba81e024407965b1592138079")
+        $AllStatsResponses = Invoke-RestMethod "$URI" -Method 'GET' -Headers $AllStatsHeaders -Body $body
+
+        foreach ($Response in $AllStatsResponses) {
             try {
                 $properties = @{
-                    Cases     = $Response.cases
-                    Deaths    = $Response.Deaths
-                    Recovered = $Response.Recovered
-                    Updated   = $EpochStart.AddMilliseconds($Response.Updated)
-                    Active    = $Response.Active
+                    active                 = [long]$Response.active
+                    activePerOneMillion    = [double]$Response.activePerOneMillion
+                    affectedCountries      = [long]$Response.affectedCountries
+                    cases                  = [long]$Response.cases
+                    casesPerOneMillion     = [long]$Response.casesPerOneMillion
+                    critical               = [long]$Response.critical
+                    criticalPerOneMillion  = [double]$Response.criticalPerOneMillion
+                    deaths                 = [long]$Response.deaths
+                    deathsPerOneMillion    = [double]$Response.deathsPerOneMillion
+                    oneCasePerPeople       = [long]$Response.oneCasePerPeople
+                    oneDeathPerPeople      = [long]$Response.oneDeathPerPeople
+                    oneTestPerPeople       = [long]$Response.oneTestPerPeople
+                    population             = [long]$Response.population
+                    recovered              = [long]$Response.recovered
+                    recoveredPerOneMillion = [double]$Response.recoveredPerOneMillion
+                    tests                  = [long]$Response.tests
+                    testsPerOneMillion     = [double]$Response.testsPerOneMillion
+                    todayCases             = [long]$Response.todayCases
+                    todayDeaths            = [long]$Response.todayDeaths
+                    todayRecovered         = [long]$Response.todayRecovered
+                    updated                = [long]$Response.updated
                 }
             }
             catch {
                 $properties = @{
-                    Cases     = $Response.cases
-                    Deaths    = $Response.Deaths
-                    Recovered = $Response.Recovered
-                    Updated   = $EpochStart.AddMilliseconds($Response.Updated)
-                    Active    = $Response.Active
+                    active                 = [long]$Response.active
+                    activePerOneMillion    = [double]$Response.activePerOneMillion
+                    affectedCountries      = [long]$Response.affectedCountries
+                    cases                  = [long]$Response.cases
+                    casesPerOneMillion     = [long]$Response.casesPerOneMillion
+                    critical               = [long]$Response.critical
+                    criticalPerOneMillion  = [double]$Response.criticalPerOneMillion
+                    deaths                 = [long]$Response.deaths
+                    deathsPerOneMillion    = [double]$Response.deathsPerOneMillion
+                    oneCasePerPeople       = [long]$Response.oneCasePerPeople
+                    oneDeathPerPeople      = [long]$Response.oneDeathPerPeople
+                    oneTestPerPeople       = [long]$Response.oneTestPerPeople
+                    population             = [long]$Response.population
+                    recovered              = [long]$Response.recovered
+                    recoveredPerOneMillion = [double]$Response.recoveredPerOneMillion
+                    tests                  = [long]$Response.tests
+                    testsPerOneMillion     = [double]$Response.testsPerOneMillion
+                    todayCases             = [long]$Response.todayCases
+                    todayDeaths            = [long]$Response.todayDeaths
+                    todayRecovered         = [long]$Response.todayRecovered
+                    updated                = [long]$Response.updated
                 }
             }
             finally {
