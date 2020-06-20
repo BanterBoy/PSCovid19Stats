8d736332b659006b5bc698d2bb99519eea749b1f
Luke Leigh <luke.leigh@gmail.com>
2020-06-21 00:25:36 +0100

    Update Get-CovidlUSCountyData.ps1

--- v2/Functions/Public/JHUCSSE/Get-CovidlUSCountyData.ps1
+++ v2/Functions/Public/JHUCSSE/Get-CovidlUSCountyData.ps1
@@ -9,17 +9,7 @@ https://corona.lmao.ninja/v2/jhucsse/counties
 
 Get JHU CSSE county specific data. This includes confirmed cases, deaths, recovered, and coordinates. Data is updated every 10 minutes
 
-GET Single County Data
-https://corona.lmao.ninja/v2/jhucsse/counties/:countyName
-Get JHU CSSE county specific data. This includes confirmed cases, deaths, recovered, and coordinates. Returns array because there are duplicate names. Data is updated every 10 minutes.
-
-PATH VARIABLES
-countyNameAbbeville
-
 #>
-
-
-# GET All US County Data
 
 $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
 $headers.Add("Cookie", "__cfduid=da03e729af67bf9c91ba81e024407965b1592138079")
@@ -28,10 +18,37 @@ $response = Invoke-RestMethod 'https://corona.lmao.ninja/v2/jhucsse/counties' -M
 $response | ConvertTo-Json
 
 
+<#
 GET Single County Data
+
+https://corona.lmao.ninja/v2/jhucsse/counties/:countyName
+
+Get JHU CSSE county specific data. This includes confirmed cases, deaths, recovered, and coordinates. Returns array because there are duplicate names. Data is updated every 10 minutes.
+
+PATH VARIABLES
+countyName
+
+#>
 
 $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
 $headers.Add("Cookie", "__cfduid=da03e729af67bf9c91ba81e024407965b1592138079")
 
 $response = Invoke-RestMethod 'https://corona.lmao.ninja/v2/jhucsse/counties/Abbeville' -Method 'GET' -Headers $headers -Body $body
 $response | ConvertTo-Json
+
+
+<#
+
+GET USA states JHU List
+https://corona.lmao.ninja/v2/historical/usacounties
+
+Returns a list of states that are available for querying the /historical/usacounties/{state} endpoint with.
+
+#>
+
+$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$headers.Add("Cookie", "__cfduid=da03e729af67bf9c91ba81e024407965b1592138079")
+
+$response = Invoke-RestMethod 'https://corona.lmao.ninja/v2/historical/usacounties' -Method 'GET' -Headers $headers -Body $body
+$response | ConvertTo-Json
+
